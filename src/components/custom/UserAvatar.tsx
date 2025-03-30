0e456d14727c072c4ff45614b51239684418fa19
ZanziPanziBY <zanzipanziby@gmail.com>
2025-03-30 13:53:07 +0300

    feat: add props for avatar component

--- src/components/custom/UserAvatar.tsx
+++ src/components/custom/UserAvatar.tsx
@@ -2,11 +2,24 @@
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import DefaultAvatar from "@/assets/DefaultAvatar.jpg";
 
-export function UserAvatar() {
+export function UserAvatar({
+  avatar,
+  userFirstName = "A",
+  userLastName = "V",
+}: Props) {
+  const avatarFallback: string = (
+    userFirstName.charAt(0) + userLastName.charAt(0)
+  ).toUpperCase();
   return (
     <Avatar>
-      <AvatarImage src={DefaultAvatar.src} />
-      <AvatarFallback>AV</AvatarFallback>
+      <AvatarImage src={avatar || DefaultAvatar.src} />
+      <AvatarFallback>{avatarFallback}</AvatarFallback>
     </Avatar>
   );
 }
+
+type Props = {
+  avatar?: string;
+  userFirstName?: string;
+  userLastName?: string;
+};
